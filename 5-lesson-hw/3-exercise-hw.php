d615d0f33b26269ed7ee700e5425ac166134135c
Lev Pollo <pollolev@gmail.com>
2023-01-22 21:24:59 +0300

    fix 5les-3hw

--- 5-lesson-hw/3-exercise-hw.php
+++ 5-lesson-hw/3-exercise-hw.php
@@ -4,13 +4,13 @@ $users = [
     [
         'name' => 'Nick',
         'age' => 23,
-        'passChangeDate' => '5-1-2023',
+        'passChangeDate' => '5-1-2024',
         'pass' => 'old-pass',
     ],
     [
         'name' => 'Alex',
         'age' => 30,
-        'passChangeDate' => '5-1-2022',
+        'passChangeDate' => '5-1-2023',
         'pass' => 'old-pass',
     ],
     [
@@ -21,27 +21,43 @@ $users = [
     ],
 ];
 
+
+
 function passUpdate(array &$users, $countMonth){
     $randNums  =  '1234567890';
     $randAbc = 'qwertyuiopasdfghjklzxcvbnm';
     $randSumb = '!@#$%^&*()_+~{}[]":?><;-=';
     $newPass = '';
+
+    //для теста
     $thisDate = getdate(time());
+    $thisDate['year'] = 2024;
+    $thisDate['mon'] = 3;
+    $differenceMonth = 0;
+
+    print_r(  $thisDate['year']);
     foreach ($users as &$user) {
         foreach ($user as $key=>&$value){
             if($key == 'passChangeDate'){
                 $date = explode('-',$value);
-                if($date[2] < $thisDate['year']){
-                    print_r($date[2]);
-                    $differenceMonth = $thisDate['mon'] + 12 - $date[1];
-                    if($differenceMonth >  $countMonth){
-                        for($i = 0; $i < 2; $i++){
-                            $newPass .= $randNums[rand(0,strlen($randNums)-1)].$randAbc[rand(0,strlen($randAbc)-1)].$randSumb[rand(0,strlen($randSumb)-1)].strtoupper($randAbc[rand(0,strlen($randAbc)-1)]);
-                        }
-                        $user['pass'] = str_shuffle($newPass);
-                        $newPass = '';
+                //если новый год больше
 
+                //если есть разница хотябы в один год
+                if($date[2] < $thisDate['year']){
+                    $differenceMonth = $thisDate['mon'] + 12 - $date[1];
+                }
+                //если есть разница в месяцах
+                else if ($date[1] < $thisDate['mon']){
+                    $differenceMonth = $thisDate['mon'] - $date[1];
+                }
+
+                //при разнице больше нужного числа месяцев, меняем пароль по ссылке
+                if($differenceMonth >  $countMonth){
+                    for($i = 0; $i < 2; $i++){
+                        $newPass .= $randNums[rand(0,strlen($randNums)-1)].$randAbc[rand(0,strlen($randAbc)-1)].$randSumb[rand(0,strlen($randSumb)-1)].strtoupper($randAbc[rand(0,strlen($randAbc)-1)]);
                     }
+                    $user['pass'] = str_shuffle($newPass);
+                    $newPass = '';
                 }
 
             }
